9c177a8359665706f7a220ccc7ca9a69336b8371
Kristian Montoya <kristian.s.montoya@gmail.com>
2023-05-27 10:53:59 -0600

    Add DataAccess Test Project

---
+++ DataAccess.Test/Repository/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Shouldly;
+using Xunit;
+
+namespace DataAccess.Test.Repository
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void DontTrustThisTest()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
